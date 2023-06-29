d385a0850557cbe08a5bcd3b52c7617d28e8a1cc
Em Diya <infor.emdiya@gmail.com>
2023-06-29 02:59:40 -0700

    Add Project

---
+++ app/model/index.js
@@ -0,0 +1,33 @@
+const dbConfig = require("../../config/db.config");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    port: 5432,
+    logging: false,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+testConnection()
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./test.model.js")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+const db = required("./app/model/index");
+
+db.sequelize.sync()
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err.message);
+    });
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.get("/api", (req, res) => {
+    res.json({
+        success: 1,
+        message: "Welcome To Diya E-Commerce"
+    });
+});
+
+// const PORT = process.env.PORT || 8080;
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server is running on port : `,process.env.APP_PORT);
+});
